88aebbc6af4187f58eb411ccbf1529be5d1490e8
Michal41 <michal.kanarek@gmail.com>
2025-07-04 13:13:07 +0200

    Realiztions page

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
               href="/"
             >
               <span className="text-gradient bg-gradient-to-r from-pink to-purple font-semibold">
-                HOLO
+                Systemy RFID
               </span>
             </a>
             <div className="hidden md:block">
@@ -22,39 +22,33 @@ const Navbar = () => {
                   className="relative after:rounded after:bg-purple after:absolute after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 text-black opacity-70 hover:opacity-100 px-3 py-2 rounded-md text-sm font-medium font-montserrat"
                   to="/"
                 >
-                  Home
+                  Strona Główna
                 </Link>
                 <Link
                   className="relative after:rounded after:bg-purple after:absolute after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 text-black opacity-70 hover:opacity-100 px-3 py-2 rounded-md text-sm font-medium font-montserrat"
                   to="/about"
                 >
-                  About
+                  O Nas
                 </Link>
 
                 <Link
                   className="relative after:rounded after:bg-purple after:absolute after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 text-black opacity-70 hover:opacity-100 px-3 py-2 rounded-md text-sm font-medium font-montserrat"
                   to="/gallery"
                 >
-                  Gallery
-                </Link>
-                <Link
-                  className="relative after:rounded after:bg-purple after:absolute after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 text-black opacity-70 hover:opacity-100 px-3 py-2 rounded-md text-sm font-medium font-montserrat"
-                  to="/blog"
-                >
-                  Blog
+                  Realizacje
                 </Link>
                 <Link
                   className="relative after:rounded after:bg-purple after:absolute after:h-0.5 after:w-0 after:bottom-0 after:left-0 hover:after:w-full after:transition-all after:duration-300 text-black opacity-70 hover:opacity-100 px-3 py-2 rounded-md text-sm font-medium font-montserrat"
                   to="/contact"
                 >
-                  Contact
+                  Kontakt
                 </Link>
                 <div className="items-center">
                   <a
                     href="tel:#"
                     className="transition-all duration-500ms ease-in-out hover:ease-in-out bg-transparent mt-5 py-2.5 px-4 text-base font-medium text-center text-black rounded-lg border border-purple hover:text-white border-black  hover:bg-purple"
                   >
-                    Call Us Now
+                    Zadzwoń Teraz
                   </a>
                 </div>
               </div>
@@ -90,20 +84,20 @@ const Navbar = () => {
               className="text-black opacity-50 hover:opacity-100 px-3 py-3 rounded-md text-m font-medium font-montserrat"
               href="/"
             >
-              Home
+              Strona Główna
             </a>
             <a
               className="text-black opacity-50 hover:opacity-100 px-3 py-3 rounded-md text-m font-medium font-montserrat"
               href="/about"
             >
-              About
+              O Nas
             </a>
 
             <a
               className="text-black opacity-50 hover:opacity-100 px-3 py-3 rounded-md text-m font-medium font-montserrat"
               href="/gallery"
             >
-              Gallery
+              Realizacje
             </a>
             <a
               className="text-black opacity-50 hover:opacity-100 px-3 py-3 rounded-md text-m font-medium font-montserrat"
@@ -115,14 +109,14 @@ const Navbar = () => {
               className="text-black opacity-50 hover:opacity-100 px-3 py-3 rounded-md text-m font-medium font-montserrat"
               href="/contact"
             >
-              Contact
+              Kontakt
             </a>
             <div className="items-center mt-20 py-3">
               <a
                 href="tel:#"
                 className="transition-all duration-500ms ease-in-out hover:ease-in-out w-full flex items-center justify-center px-8 py-3 text-base font-medium rounded-md text-center bg-purple text-white hover:text-black border border-purple  hover:bg-transparent md:text-lg md:px-10 "
               >
-                Call Us Now
+                Zadzwoń Teraz
               </a>
             </div>
           </div>
